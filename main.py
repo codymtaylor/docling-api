dac8af6d84b9593942b5d60ec128079b0d08fde6
Cody <codymtaylor@users.noreply.github.com>
2024-12-03 05:44:52 +0000

    Index.html now displays

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse  # Import HTMLResponse
 from document_converter.route import router as document_converter_router
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,5 +14,18 @@ app.add_middleware(
     allow_credentials=True,
 )
 
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="public"), name="static")
 
-app.include_router(document_converter_router, prefix="", tags=["document-converter"])
+
+# Serve the index.html at the root
+@app.get("/",
+         response_class=HTMLResponse)  # Set response_class to HTMLResponse
+async def read_index():
+    with open("public/index.html") as f:
+        return f.read()
+
+
+app.include_router(document_converter_router,
+                   prefix="",
+                   tags=["document-converter"])
